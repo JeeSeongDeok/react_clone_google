fc73445cac5e4040927f5dda0f8957608539fc15
unknown <bh939@naver.com>
2023-01-09 14:13:08 +0900

    first commit

---
+++ src/pages/main/App.js
@@ -0,0 +1,17 @@
+import logo from 'assets/googlelogo_color_272x92dp.png';
+import 'pages/main/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Google.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
