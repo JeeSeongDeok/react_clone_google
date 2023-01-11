0e507538f75e03bc46c5b9e35df87a51fb790970
unknown <bh939@naver.com>
2023-01-11 10:14:42 +0900

    검색창 이벤트 커밋

--- src/pages/main/App.js
+++ src/pages/main/App.js
@@ -1,12 +1,25 @@
 import logo from 'assets/googlelogo_color_272x92dp.png';
 import 'pages/main/App.css'
+import React, { useState } from 'react';
 
 function App() {
+  const [textArea, setTextArea] = useState(false);
+  const textAreaEnter = () => {
+    setTextArea(true);
+  }
+  const textAreaOut = () => {
+    setTextArea(false);
+  }
   return (
     <div className="App">
       <body className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" className="App-area" />
+        <input
+          type="text"
+          className={textArea ? "App-enter-area" : "App-area"}
+          onMouseEnter={() => textAreaEnter()}
+          onMouseLeave={() => textAreaOut()}
+        />
       </body>
     </div>
   );
